0786abc5f6a73e59c9701c78d6cc6f2df56d0661
Mau224 <46903680+Mau224@users.noreply.github.com>
2020-05-21 23:11:57 +0300

    сверстал блок с информацией

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,20 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+$(function () {
+  $('#phone').mask('+ (999)9–999–9999');
+});
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+$('body').on('click', '.tab__navitem', function (event) {
+  var eq = $(this).index();
+  if ($(this).hasClass('parent')) {
+    var eq = $(this).parent().index();
+  }
+  if (!$(this).hasClass('active')) {
+    $(this).closest('.tab').find('.tab__navitem').removeClass('active');
+    $(this).addClass('active');
+    $(this).closest('.tab').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+    if ($(this).closest('.tab').find('.slick-slider').length > 0) {
+      $(this).closest('.tab').find('.slick-slider').slick('setPosition');
+    }
   }
 });
